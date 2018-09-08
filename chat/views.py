d646d826b0bee5bd3c59731d7280cf1a4ea17dd9
John Mitchell <john@johntellsall.com>
2018-09-08 12:33:27 -0700

    template

--- chat/views.py
+++ chat/views.py
@@ -1,14 +1,22 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
 
+def old_index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
