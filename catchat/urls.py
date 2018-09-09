6a919e19c7c647b04731d192e2227edde88a1f83
John Mitchell <john@johntellsall.com>
2018-09-08 18:41:29 -0700

    partial support for user uploads

--- catchat/urls.py
+++ catchat/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
+base = [
     url(r'^chat/', include('chat.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+user_uploads = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns = base + user_uploads
