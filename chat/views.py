6cb40b80990a8b3845d1ff4accb5433b2fb3a63b
John Mitchell <john@johntellsall.com>
2018-09-15 15:22:33 -0700

    wip: 3/6 tests pass

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,24 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.utils import timezone
+from django.views import generic
 
 from .models import Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 def old_index(request):
